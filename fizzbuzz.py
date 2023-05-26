fcd4275c2a9ff3bd3f3db5ad814256091af14910
isahua16 <isaelhuard@gmail.com>
2023-05-25 20:59:35 -0600

    Created a fizzbuzz function

---
+++ fizzbuzz.py
@@ -0,0 +1,24 @@
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        print("Fizzbuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+
+
+fizzbuzz(9)
+fizzbuzz(13)
+fizzbuzz(18)
+fizzbuzz(20)
+fizzbuzz(25)
+fizzbuzz(30)
+fizzbuzz(32)
+fizzbuzz(33)
+fizzbuzz(35)
+fizzbuzz(38)
+fizzbuzz(42)
+fizzbuzz(46)
+fizzbuzz(50)
+fizzbuzz(51)
+fizzbuzz(51)
