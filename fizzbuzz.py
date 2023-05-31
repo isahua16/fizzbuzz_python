2aafa58ab6cdaa59840872a10940ccc5dc6e0d38
isahua16 <isaelhuard@gmail.com>
2023-05-30 21:27:17 -0600

    Refactor so the function accepts a list of numbers

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,24 +1,13 @@
-def fizzbuzz(number):
-    if number % 3 == 0 and number % 5 == 0:
-        print("Fizzbuzz")
-    elif number % 3 == 0:
-        print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
+def fizzbuzz(arr):
+    for number in arr:
+        if number % 3 == 0 and number % 5 == 0:
+            print("Fizzbuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
 
 
-fizzbuzz(9)
-fizzbuzz(13)
-fizzbuzz(18)
-fizzbuzz(20)
-fizzbuzz(25)
-fizzbuzz(30)
-fizzbuzz(32)
-fizzbuzz(33)
-fizzbuzz(35)
-fizzbuzz(38)
-fizzbuzz(42)
-fizzbuzz(46)
-fizzbuzz(50)
-fizzbuzz(51)
-fizzbuzz(51)
+nums = [9, 13, 18, 20, 25, 30, 32, 33, 35, 38, 42, 46, 50, 51, 58, 67]
+
+fizzbuzz(nums)
