4212565f83b143c159517f19617c2826b83a66d0
isahua16 <isaelhuard@gmail.com>
2023-05-30 21:32:21 -0600

    add fizzbuzz function to except one number only and loop outside the function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,6 +8,18 @@ def fizzbuzz(arr):
             print("Buzz")
 
 
+def fizzbuzz1(number):
+    if number % 3 == 0 and number % 5 == 0:
+        print("Fizzbuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+
+
 nums = [9, 13, 18, 20, 25, 30, 32, 33, 35, 38, 42, 46, 50, 51, 58, 67]
 
 fizzbuzz(nums)
+
+for num in nums:
+    fizzbuzz1(num)
